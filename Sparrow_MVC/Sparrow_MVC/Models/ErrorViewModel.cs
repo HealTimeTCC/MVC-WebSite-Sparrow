2d850064ce3c6d19f6a0d0187b43c5fdec9d96a6
DanMarzo <marzogildan@gmail.com>
2023-01-10 23:08:36 -0300

    Inicio: defeito no Navbar

---
+++ Sparrow_MVC/Sparrow_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sparrow_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
