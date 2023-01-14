0ee392bf5e3cd82a229e0d32f54d11ce684c8ce6
DanMarzo <marzogildan@gmail.com>
2023-01-13 23:25:57 -0300

    Feito a reconstrução do menu superior

---
+++ Sparrow_MVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sparrow_MVC.Models;
+using System.Diagnostics;
+
+namespace Sparrow_MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async Task<IActionResult> IndexAsync()
+    {
+        return View();
+    }
+    
+}
---
+++ Sparrow_MVC/Controllers/ProdutosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sparrow_MVC.Controllers;
+
+public class ProdutosController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
